e767852ad0ef2677643ebe5fd0ba9af6cd45c36a
ahauserus <ahauserus@gmail.com>
2023-06-15 10:28:53 +0300

    writes to stderr without stdio.h

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - Entry point of the program
+ * Return: 0 is success, 1 otherwise
+ */
+
+int main(void)
+{
+	char *string = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int bytes_written	 = write(2, string, strlen(string));
+
+	if (bytes_written == -1)
+	{
+		return (1);
+	}
+
+	return (0);
+}
