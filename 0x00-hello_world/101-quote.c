b108498b6601c24600b7baed385fe9e7f55a4b8f
ahauserus <ahauserus@gmail.com>
2023-06-15 10:31:44 +0300

    writes to stderr without stdio.h

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	char *string;
 
-	str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	string = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	int bytes_written	 = write(2, string, strlen(string));
 
 	if (bytes_written == -1)
