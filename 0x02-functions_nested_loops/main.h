3d5bc6136c64d42d27cfd7fff695b9b015d5918c
ahauserus <ahauserus@gmail.com>
2023-06-20 16:07:46 +0300

    updated _abs(int num)

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,7 +7,7 @@ void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int _abs(int);
+int _abs(int num);
 int print_last_digit(int);
 int jack_bauer(void);
 int times_table(void);
