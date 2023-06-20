b870820c0de6ee2041cf8fe8e9aa5c275296de64
ahauserus <ahauserus@gmail.com>
2023-06-20 19:02:39 +0300

    added args in prototype add

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,7 +11,7 @@ int _abs(int num);
 int print_last_digit(int num);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int a, int b);
 int print_to_98(int n);
 int print_times_table(int n);
 
