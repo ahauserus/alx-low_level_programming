b788132eb110c8267ac34abc0ee20760a42d07d9
ahauserus <ahauserus@gmail.com>
2023-06-22 02:14:21 +0300

    tests positive_or_negative()

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests positive_or_negative()
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
