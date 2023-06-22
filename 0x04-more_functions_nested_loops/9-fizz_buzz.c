7ba684195140629d63c8579a4f79c59377279f1d
ahauserus <ahauserus@gmail.com>
2023-06-22 22:43:57 +0300

    the fizzbuzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - The FizzBuzz Test
+ * Return: 0 is success, 1 otherwise
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	putchar(10);
+
+	return (0);
+}
