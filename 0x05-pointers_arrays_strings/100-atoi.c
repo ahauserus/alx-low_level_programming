90443d12543c23fc8ca2c5487c5e1521f4524e2f
Ahauserus <ahauserus@gmail.com>
2023-06-27 00:27:02 +0300

    convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,18 +9,25 @@
 
 int _atoi(char *s)
 {
-	int pre = 1;
-	unsigned int i = 0;
+	int i, j = 0, k = -1;
 
-	while (*s++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		if (*s == '-')
-			pre *= -1;
-		else if (*s >= '0' && *s <= '9')
-			i = (i * 10) + (*s - '0');
-		else if (i > 0)
-			break;
+		if (*(s + i) == '-')
+			k *= -1;
+		if (*(s + i) > 47 && *(s + i) < 58)
+		{
+			if (j < 0)
+				j = (j * 10) - (*(s + i) - '0');
+			else
+				j = (s[i] - '0') * -1;
+			if (s[i + 1] < 48 || s[i] > 57)
+				break;
+		}
 	}
+	if (k < 0)
+		j *= -1;
 
-	return (pre * i);
+	return (j);
+
 }
