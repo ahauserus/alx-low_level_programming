a1d2f252bb90677d900db193dc5f29cf7c7ae597
Ahauserus <ahauserus@gmail.com>
2023-06-30 01:21:12 +0300

    ncodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @str: string to be encoded
+ *
+ * Return: pointer to the encoded string
+ */
+
+char *rot13(char *str)
+{
+	int i, j;
+
+	char enc[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+	char dec[] = "nNpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+
+	for (i = 0; *(str + i); i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (enc[j] == *(str + i))
+				*(str + i) = *(dec + j);
+			break;
+		}
+	}
+
+	return (str);
+}
