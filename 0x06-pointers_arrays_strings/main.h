950d45476fedd4c76048dfc297d59cc434d2371b
Ahauserus <ahauserus@gmail.com>
2023-06-30 05:50:56 +0300

    header file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,5 +14,7 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+void *_memmove(const void *dest, const void *src, unsigned long int n);
+int _strlen(char *s);
 
 #endif
