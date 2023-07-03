1c77859acc635ecd33d05365c07919503b823710
Ahauserus <ahauserus@gmail.com>
2023-07-03 23:55:43 +0300

    locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,12 @@
 #include "main.h"
 
-/**/
+/**
+ * _strstr - locates a substring
+ * @haystack: The string to search
+ * @needle: The substring to match
+ *
+ * Return: A pointer to the first occurrence of needle in haystack
+ */
 
 char *_strstr(char *haystack, char *needle)
 {
