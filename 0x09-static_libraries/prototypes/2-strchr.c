939b4603639924d74a3d0db589ee69f5b9b24f70
Ahauserus <ahauserus@gmail.com>
2023-07-07 17:03:37 +0300

    duplicates a str

---
+++ 0x09-static_libraries/prototypes/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: The string to search
+ * @c: The character to be located
+ *
+ * Return: A pointer to the first occurrence of c, NULL otherwise
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; *(s + i) >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+
+	return ('\0');
+}
