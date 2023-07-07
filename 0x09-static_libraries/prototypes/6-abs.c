a5294aa3de0004e66d6e0688749582fc8cb5d90f
Ahauserus <ahauserus@gmail.com>
2023-07-07 17:06:51 +0300

    copies a str

---
+++ 0x09-static_libraries/prototypes/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @num: Integer to be computed
+ *
+ * Return: The absolute value of num
+ */
+
+int _abs(int num)
+{
+	return (num < 0 ? -num : num == 0 ? 0 : num);
+}
