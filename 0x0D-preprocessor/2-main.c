978f050ce47791e65128e192df91cd02f60534e3
Ahauserus <ahauserus@gmail.com>
2023-07-17 17:54:32 +0300

    prints the name of the file it was compiled from, followed by a new line

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file, followed by a new line
+ *
+ * Return: Always 0 (Success), 1 (Failure)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
