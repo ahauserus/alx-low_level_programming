73e4c051e7fd2ac961ab93e5f8cc907c465e5542
Ahauserus <ahauserus@gmail.com>
2023-07-18 00:11:42 +0300

    header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,10 @@ struct dog
 	char *owner;
 };
 
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
