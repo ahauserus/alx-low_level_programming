3a905f84c093863bbc429e16dae6e8c9d8b9338e
Ahauserus <ahauserus@gmail.com>
2023-07-20 01:01:16 +0300

    performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,15 +19,17 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	num1 = atoi(argv[1]), op = argv[2], num2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
 
-	if (get_op_func(op) == NULL || op[1] == '\0')
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*op == '/' || *op == '%') && num2 == 0)
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
