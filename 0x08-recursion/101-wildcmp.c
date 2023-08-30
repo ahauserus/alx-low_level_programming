a2b8d8361263960e65bea9f6f5adca5c5af18aff
ahauserus <ahauserus@gmail.com>
2023-08-30 21:48:20 +0300

    compare recursively

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -28,7 +28,7 @@ int wildcmp(char *s1, char *s2)
 	else if (*s1 == '\0')
 		return (*s2 == '\0');
 
-	else if (*s1 == *s2 || *s2 == ' ')
+	else if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 
 	return (0);
